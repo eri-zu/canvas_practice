9de190062d7ba265021709417d514b46d8552f5b
Eri_Tsutsui <tsutsui@balance.bz>
2022-07-24 13:06:13 +0900

    add: 跳ね返り

--- lesson1/js/Ball.js
+++ lesson1/js/Ball.js
@@ -2,24 +2,32 @@ import Base from "./Base.js";
 import * as m from "./Math.js";
 
 export default class Ball extends Base {
-  constructor(canvas, ctx, i) {
+  constructor(canvas, ctx, num, i) {
     super();
 
     this.canvas = canvas;
     this.ctx = ctx;
+    this.num = num;
     this.i = i;
+    this.w = this.canvas.width;
+    this.h = this.canvas.height;
+
+    // ボール
+    this.r = 10; // 半径
+
+    // 5発ランダム
     this.num = 5;
-
     this.speed = Math.random() * 5;
-    this.life = 1;
-    this.lifespan = 2; // 2sで消える
-    this.frame = 0;
-    this.op = 1;
-
     this.vector2 = {
       x: Math.cos(m.radian((360 / this.num) * this.i)),
       y: Math.sin(m.radian((360 / this.num) * this.i)),
     };
+
+    // 寿命
+    this.frame = 0;
+    this.life = 1;
+    this.lifespan = 6; // 6sで消える
+    this.op = 1;
 
     this.isUEv = true;
 
@@ -37,18 +45,30 @@ export default class Ball extends Base {
   update() {
     this.frame++;
 
-    if (this.frame % (60 * this.lifespan) == 0) {
-      this.life = 0;
-      this.op = this.life;
-    }
+    // 寿命
+    if (this.dead()) this.life = this.op = 0;
+
+    // 端で跳ね返り
+    this.turn();
 
     this.posX += this.speed * this.vector2.x;
     this.posY += this.speed * this.vector2.y;
 
     this.ctx.beginPath();
-    this.ctx.arc(this.posX, this.posY, 10, 0, Math.PI * 2, true);
-    this.ctx.fillStyle = `rgba(0, 0, 0, ${this.op}`;
+    this.ctx.arc(this.posX, this.posY, this.r, 0, Math.PI * 2, true);
+    this.ctx.fillStyle = `rgba(0, 0, 0, ${this.op})`;
     this.ctx.fill();
+  }
+
+  dead() {
+    return this.frame % (60 * this.lifespan) == 0 ? true : false;
+  }
+
+  turn() {
+    if (this.posX - this.r <= 0 || this.posX + this.r >= this.w)
+      this.speed *= -1;
+    if (this.posY - this.r <= 0 || this.posY + this.r >= this.h)
+      this.speed *= -1;
   }
 
   setEvents() {
--- lesson1/js/CanvasController.js
+++ lesson1/js/CanvasController.js
@@ -16,15 +16,29 @@ export default class CanvasController extends Base {
   setup() {
     this.canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
+
+    this.balls = [];
+    this.num = 5; // ボール数
   }
 
   update() {
+    // clear
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    for (let i = this.balls.length - 1; i >= 0; i--) {
+      // 描画
+      this.balls[i].update();
+
+      // canvas上から消す
+      if (this.balls[i].dead()) this.balls.splice(i, 1); // splice(start, count)
+    }
   }
 
   createBall(e) {
-    for (let i = 0; i < 5; i++) {
-      const ball = new Ball(this.canvas, this.ctx, i);
+    for (let i = 0; i < this.num; i++) {
+      const ball = new Ball(this.canvas, this.ctx, this.num, i);
+      this.balls.push(ball);
+
       ball.getPos(e);
     }
   }
