dcabe336845da10f8cac53dcfe59ba9130114663
Eri_Tsutsui <tsutsui@balance.bz>
2022-07-24 19:29:02 +0900

    add: 減速

--- lesson1/js/Ball.js
+++ lesson1/js/Ball.js
@@ -17,18 +17,29 @@ export default class Ball extends Base {
 
     // 5発ランダム
     this.num = 5;
-    this.speed = Math.random() * 5;
+    this.speed = Math.random() * 10; // 1 - 10 小数点
+
     this.vector2 = {
       x: Math.cos(m.radian((360 / this.num) * this.i)),
       y: Math.sin(m.radian((360 / this.num) * this.i)),
     };
 
+    // 減速
+    this.a = 0.99;
+
     // 寿命
     this.frame = 0;
     this.life = 1;
-    this.lifespan = 6; // 6sで消える
+    this.lifespan = 20; // 20sで消える
     this.op = 1;
     this.isDead = false;
+
+    // 色
+    this.color = {
+      r: Math.round(Math.random() * 255),
+      g: Math.round(Math.random() * 255),
+      b: Math.round(Math.random() * 255),
+    };
 
     this.isUEv = true;
 
@@ -52,12 +63,18 @@ export default class Ball extends Base {
     // 端で跳ね返り
     this.turn();
 
+    // 寿命の半分時間経過したら2frameに一回減速開始
+    if (this.frame >= 60 * (this.lifespan / 2) && this.frame % 2 == 0)
+      this.speed *= this.a;
+
+    // 移動
     this.posX += this.speed * this.vector2.x;
     this.posY += this.speed * this.vector2.y;
 
+    // 描画
     this.ctx.beginPath();
     this.ctx.arc(this.posX, this.posY, this.r, 0, Math.PI * 2, true);
-    this.ctx.fillStyle = `rgba(0, 0, 0, ${this.op})`;
+    this.ctx.fillStyle = `rgba(${this.color.r}, ${this.color.g}, ${this.color.b}, ${this.op})`;
     this.ctx.fill();
   }
 
@@ -67,10 +84,13 @@ export default class Ball extends Base {
   }
 
   turn() {
-    if (this.posX - this.r <= 0 || this.posX + this.r >= this.w)
+    if (this.posX - this.r <= 0 || this.posX + this.r >= this.w) {
       this.speed *= -1;
-    if (this.posY - this.r <= 0 || this.posY + this.r >= this.h)
+    }
+
+    if (this.posY - this.r <= 0 || this.posY + this.r >= this.h) {
       this.speed *= -1;
+    }
   }
 
   setEvents() {
